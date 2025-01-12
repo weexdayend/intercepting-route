11fc5e2b8a3d283bdc36a94253d0abff75b9fe2f
Irvan Gerhana <irvangerhana@Irvans-MacBook-Air.local>
2025-01-12 21:16:31 +0700

    interceptng route

---
+++ src/app/@modal/(.)login/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React, { useEffect, useRef } from 'react'
+import { useRouter } from 'next/navigation'
+
+type Props = {}
+
+function Page({}: Props) {
+  const router = useRouter()
+  const dialogRef = useRef<HTMLDialogElement>(null)
+
+  useEffect(() => {
+    dialogRef.current?.showModal()
+  }, [])
+  
+  return (
+    <dialog
+      ref={dialogRef}
+      onClose={() => router.back()}
+      className='border p-4 rounded background:bg-slate-300/75'
+    >
+      <button onClick={() => router.back()}>Close this</button>
+      <h1>Intercepted Login Page</h1>
+    </dialog>
+  )
+}
+
+export default Page
---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Page({}: Props) {
+  return (
+    <div>Login Page</div>
+  )
+}
+
+export default Page
